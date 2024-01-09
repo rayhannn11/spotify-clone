9ffe9b3c2f4f04addef2df37bd6e61d701ab4456
rayhannn11 <rayhan.onepiece@gmail.com>
2024-01-09 12:06:08 +0700

    finish project

---
+++ hooks/useOnPlay.ts
@@ -0,0 +1,24 @@
+import { Song } from '@/types';
+import { useUser } from '@supabase/auth-helpers-react';
+
+import usePlayer from './usePlayer';
+import useAuthModal from './useAuthModal';
+
+const useOnPlay = (songs: Song[]) => {
+  const player = usePlayer();
+  const authModal = useAuthModal();
+  const user = useUser();
+
+  const onPlay = (id: string) => {
+    if (!user) {
+      return authModal.onOpen();
+    }
+
+    player.setId(id);
+    player.setIds(songs.map((song) => song.id));
+  };
+
+  return onPlay;
+};
+
+export default useOnPlay;
